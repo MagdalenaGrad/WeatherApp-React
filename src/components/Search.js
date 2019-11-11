d56a8df412f4c8477ae50da1888f03f3bb07da4e
Magdalena Grad <mgdln.grad@gmail.com>
2019-11-11 22:41:52 +0100

    add styles for searchbar

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import './Search.css';
 
 const Search = (props) => {
   return (
-    <>
-      <form className="SearchBar" onSubmit={props.submit}>
-        <input type="text" placeholder="city" onChange={props.change}></input>
+    <div className="wrap">
+      <div className="header">
+        <h1>Weather App</h1>
+      </div>
+      <form className="form-search" onSubmit={props.submit}>
+        <input className="search-bar" type="text" placeholder="Your city, e.g. Warsaw" onChange={props.change}></input>
+        <button onSubmit={props.submit}><i className="fas fa-search"></i></button>
       </form>
-    </>
+    </div>
   )
 }
 
