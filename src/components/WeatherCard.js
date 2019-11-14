de04bcb59637d551e4f65f1cfbb0a97260017dcf
Magdalena Grad <mgdln.grad@gmail.com>
2019-11-14 12:11:15 +0100

    add grid for minmax temp

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -6,26 +6,36 @@ const WeatherCard = (props) => {
 
   const { error, date, sunrise, sunset, value, city, temp, pressure, wind, tempMin, tempMax, weather, weatherDesc } = props.weatherData;
 
+
+  const sunriseTime = new Date(sunrise).toLocaleTimeString();
+  const sunsetTime = new Date(sunset).toLocaleTimeString();
+
+  const contentCard = null;
+
   return (
     <div className="card">
       <h4><span className="city-name">{city}</span></h4>
       <div>
-        <div className="weather-data"></div>
         <div className="weather-main">
+          <img className="icon-weather" src={sun}></img>
           <div className="weather-desc">
-            <p><img className="icon-weather" src={sun}></img></p>
             <p><strong>{weatherDesc}</strong></p>
           </div>
-          <div className="temperature">{temp} &#176;C</div>
         </div>
-        <div className="minmax">
-          <p>minimalna: {tempMin}</p>
-          <p>maksymalna: {tempMax}</p>
+        <div className="weather-temp">
+          <div className="temperature">{temp} &#176;C</div>
+          <div className="minmax">
+            <div className="min">min</div>
+            <div className="temp-min">{tempMin}</div>
+            <div className="minmax-icon"><i class="fas fa-temperature-high"></i></div>
+            <div className="max">max</div>
+            <div className="temp-max">{tempMax}</div>
+          </div>
         </div>
       </div>
       <div className="other-data">
-        <p>wschód słońca: {sunrise}</p>
-        <p>zachód słońca: {sunset}</p>
+        <p>wschód słońca: {sunriseTime}</p>
+        <p>zachód słońca: {sunsetTime}</p>
         <p>Ciśnienie: {pressure} hPa</p>
       </div>
     </div>
